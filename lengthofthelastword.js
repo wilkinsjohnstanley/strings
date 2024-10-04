a95363f98f139b22b5b6ac6dc3facd24ee52fbcd
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-10-04 17:22:14 -0500

    solved my first leetcode problem

--- lengthofthelastword.js
+++ lengthofthelastword.js
@@ -9,11 +9,14 @@ Input: s = "Hello World"
 Output: 5
 Explanation: The last word is "World" with length 5. */
 const lengthOfTheLastWord = function(string){
-    let sentence = string.split(" ") //a sentence would be the string split on a space
+    let sentence = string
+    .trim()
+    .split(" ") //a sentence would be the string split on a space
+    
     //console.log(sentence)
     let lastWord = sentence.length - 1
     return sentence[lastWord].length
     //console.log(lastWord)
 
 }
-console.log(lengthOfTheLastWord("Hello World"))
+console.log(lengthOfTheLastWord("   fly me   to   the moon  "))
