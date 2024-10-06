f48f9d6faeb9efd2d869354f15357a89520c7872
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-10-06 14:47:38 -0500

    needle in the hay

--- 28-FindTheIndexoftheFirstOccurrernceinaString.js
+++ 28-FindTheIndexoftheFirstOccurrernceinaString.js
@@ -11,21 +11,30 @@
  * 
  * 
  */
-const strStr = function(haystack, needle) {
-    // If the needle is empty, return 0
-    if (needle === "") return 0;
+// const strStr = function(haystack, needle) {
+//     // If the needle is empty, return 0
+//     if (needle === "") return 0;
     
-    // Iterate through the haystack
-    for (let i = 0; i <= haystack.length - needle.length; i++) {
-        // Check if the substring starting at index i matches the needle
-        if (haystack.substring(i, i + needle.length) === needle) {
-            return i;
-        }
-    }
+//     // Iterate through the haystack
+//     for (let i = 0; i <= haystack.length - needle.length; i++) {
+//         // Check if the substring starting at index i matches the needle
+//         if (haystack.substring(i, i + needle.length) === needle) {
+//             return i;
+//         }
+//     }
     
-    // If no match is found, return -1
-    return -1;
-};
+//     // If no match is found, return -1
+//     return -1;
+// };
+var strStr = function(haystack, needle) {
+
+    first = haystack.indexOf(needle)
+  
+
+    
+    return first;
+   
+      }  
 
 // Example usage
 console.log(strStr("hello", "ll")); // Output: 2
